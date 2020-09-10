a0dfb305a03aadde545767cd746e705798874789
Alan <alanrd772@gmail.com>
2020-09-11 01:32:06 +0400

    updated to reflect changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 document.body.scrollTop = 0; // scroll to top
 
-// parallax scroll when the page is loaded so it doesn't jump
+// parallax scroll when the page is loaded so it doesn't jump when u scroll
 document.body.style.backgroundPositionY = `${window.scrollY * -0.1}px`;
